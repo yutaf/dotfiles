a830a8f75ea0299380731027e60d69d8bc9a00b8
yutaf <yutafuji2008@gmail.com>
2013-12-16 16:47:59 +0900

    Update home/.bashrc.linux.sh

--- home/.bashrc.linux.sh
+++ home/.bashrc.linux.sh
@@ -10,17 +10,11 @@ fi
 #alias mv='mv -i'
 
 PATH="$PATH":/usr/local/mysql/bin:/usr/local/php-5.4.10/bin
-if [ `hostname` = "coyomie.jp" ]; then
-	export PATH=~/bin/git-1.8.5.1/bin:$PATH # for git
-fi
 
 # for shortcut
 alias lsZ='ls -lZ'
 alias lsdZ='ls -ldZ'
 alias listusers="cut -d: -f1 /etc/passwd"
-if [ `whoami` = "git" ]; then
-	alias sshcoyomie.jp='ssh git@219.94.168.193'
-fi
 
 # Terminal Prompt
 PS1="\n\`if [ \$? = 0 ]; then echo \[\e[32m\]^_^\[\e[0m\]; else echo \[\e[31m\]O_O\[\e[0m\]; fi\` (\u@\h:\w)\n* "
