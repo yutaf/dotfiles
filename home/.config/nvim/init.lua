7ce8beefd4ca8bd24b4cf5129f67517225f40534
yutaf <yutafuji2008@gmail.com>
2026-03-07 23:16:06 +0900

    Update nvim basic settings and solarized scheme

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,7 +1,10 @@
 vim.cmd("source ~/vimrc/common.vim")
 
 vim.g.mapleader = " "
+
 vim.opt.termguicolors = true
+vim.opt.runtimepath:append(vim.fn.stdpath("data") .. "/site")
+vim.env.PATH = "/usr/local/bin:" .. vim.env.PATH
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local uv = vim.uv or vim.loop
@@ -18,7 +21,30 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  { "altercation/vim-colors-solarized" },
+  {
+    "Tsuzat/NeoSolarized.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("NeoSolarized").setup({
+        style = "dark",
+        transparent = false,
+        terminal_colors = true,
+        enable_italics = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = { bold = true },
+          variables = {},
+          string = { italic = true },
+          underline = true,
+          undercurl = true,
+        },
+      })
+      vim.cmd.colorscheme("NeoSolarized")
+    end,
+  },
+
   { "tpope/vim-fugitive" },
   { "tpope/vim-surround" },
   { "mattn/emmet-vim" },
@@ -38,11 +64,11 @@ require("lazy").setup({
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
   },
+}, {
+  rocks = {
+    enabled = false,
+  },
 })
-
-if vim.fn.isdirectory(vim.fn.expand("~/.local/share/nvim/lazy/vim-colors-solarized")) == 1 then
-  vim.cmd("colorscheme solarized")
-end
 
 vim.g.copilot_no_tab_map = true
 vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
@@ -51,9 +77,7 @@ vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
   silent = true,
 })
 
-local lspconfig = require("lspconfig")
-
-lspconfig.gopls.setup({
+vim.lsp.config("gopls", {
   settings = {
     gopls = {
       gofumpt = true,
@@ -65,6 +89,8 @@ lspconfig.gopls.setup({
   },
 })
 
+vim.lsp.enable("gopls")
+
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
@@ -75,6 +101,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local opts = { buffer = ev.buf, silent = true }
+
     vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
